5c2812e1168bd05d4a15fe38ac968f87976aa821
koji-torishima <torikou3685@gmail.com>
2020-07-08 16:27:18 +0900

    Initial Commit

---
+++ Xode_Previews_UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xode_Previews_UIkit
+//
+//  Created by koji torishima on 2020/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
